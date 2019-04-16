6f9ebab20831f2028f5913119d9c8a54373375a0
Newworldco <taishuaihu@gmail.com>
2019-04-16 16:27:55 +0800

    add house price excel

--- beautifulsp/houseprice.py
+++ beautifulsp/houseprice.py
@@ -1,7 +1,7 @@
 import requests
 import bs4
 import re
-
+import openpyxl
 
 def open_url(url):
     headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
@@ -16,23 +16,32 @@ def find_date(res):
     content = soup.find(class_="fjlist-box boxstyle1")
     target = content.find_all("li")
     for each in target:
-        data.append(each.text.strip() + '\n')
+        data.append([each.b.text.strip()[5:] + '\n'] + [each.span.text] + [each.em.text])
 
+    print(data)
     return data
 
 
-    # print(data)
 
+def to_excel(data):
+    wb = openpyxl.Workbook()
+    wb.guess_types = True
+    ws = wb.active
+    ws.append(['城市', '房价', '走势'])
+    for each in data:
+        ws.append(each)
+
+    wb.save("2019全国房价走势.xlsx")
 
 def main():
     url = "https://www.anjuke.com/fangjia/quanguo2019/"
     res = open_url(url)
     dates = find_date(res)
+    to_excel(dates)
 
-    with open("2019年各城市房价.txt", "w", encoding="utf-8")as file:
-        for each in dates:
-            file.write(each)
-
+    # '''with open("2019年各城市房价.txt", "w", encoding="utf-8")as file:
+    #        for each in dates:
+    #         file.write(each
 
 
 if __name__ == '__main__':
